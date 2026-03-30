cdaec3501d7048c486ff46b9a4e21bf021fe05b0
Your Name <kacper.ksiazek32@gmail.com>
2026-03-30 13:43:59 +0200

    1st commit

--- face_det.py
+++ face_det.py
@@ -1,19 +1,19 @@
 import cv2
 import matplotlib.pyplot as plt
 
-img = cv2.imread('360_F_530010960_WkmW6q8FIZ42LkEQoSRCLTNTw0WgJqKY.jpg')
+img = cv2.imread('smooth_skin_before.jpg')
 RGB_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades +'haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye_tree_eyeglasses.xml')
 
-faces = face_cascade.detectMultiScale(RGB_img, 1.5, 6)
+faces = face_cascade.detectMultiScale(RGB_img, 1.6, 6)
 
 for (x, y, w, h) in faces:
      cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
      ROI_img = RGB_img[y:y+w, x:x+w]
      ROI_color = img[y:y+w, x:x+h]
-     eyes = eye_cascade.detectMultiScale(ROI_img,1.5,6)
+     eyes = eye_cascade.detectMultiScale(ROI_img,1.6,6)
 
      for (ex, ey, ew, eh) in eyes:
          eye_center = (x+ ex + ew//2, y + ey + eh//2)
--- video_face.py
+++ video_face.py
@@ -1,10 +1,22 @@
+import os
+os.environ["OPENCV_VIDEOIO_MSMF_ENABLE_HW_TRANSFORMS"] = "0"
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
 
-cap = cv2.VideoCapture('kolonko.mp4')
+s = 0
+if len(sys.argv) > 1:
+    s = int(sys.argv[1])
 
-if not cap.isOpened():
+source = cv2.VideoCapture(0)
+
+source.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*"MJPG"))
+source.set(cv2.CAP_PROP_FPS, 60)
+source.set(cv2.CAP_PROP_FRAME_HEIGHT, 1050)
+source.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+
+if not source.isOpened():
     print("Error: Could not open video file.")
     exit()
 
@@ -12,35 +24,24 @@ face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades +'haarcascade_frontal
 eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye_tree_eyeglasses.xml')
 
 while True:
-    ret, frame = cap.read()
+    ret, frame = source.read()
     if (ret == True):
 
         gray_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        faces = face_cascade.detectMultiScale(gray_img, 1.05, 5)
+        faces = face_cascade.detectMultiScale(gray_img, 1.3, 4)
 
         for (x, y, w, h) in faces:
              cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
              ROI_img = gray_img[y:y+w, x:x+w]
              ROI_color = frame[y:y+w, x:x+h]
-             eyes = eye_cascade.detectMultiScale(ROI_img,1.05,5)
+             eyes = eye_cascade.detectMultiScale(ROI_img,1.1,6)
 
              for (ex, ey, ew, eh) in eyes:
                  cv2.circle(ROI_color, (ex + 30, ey + 30), int((ey/2)), (0, 255, 0), 2)
-        if (h):
-            print("active")
-            print(ret)
 
-        else:
-            print("inactive")
-            print(ret)
+        cv2.imshow('0', frame)
 
-        cv2.imshow('1', frame)
-
-        if cv2.waitKey(10) & 0xFF == ord('q'):
+        if cv2.waitKey(5) & 0xFF == ord('q'):
             break
 
-    cap.release()
-    #cv2.destroyAllWindows()
-# plt.imshow(RGB_img)
-# plt.waitforbuttonpress()
-# plt.close('all')
+source.release()
